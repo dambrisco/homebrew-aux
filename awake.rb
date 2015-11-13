b6ca9dc2f1d1f003ae3bfc2d7dc7f0adda5e6cca
Dan Ambrisco <dambrisco@itriagehealth.com>
2015-11-12 19:05:53 -0700

    Update formula for awake

--- awake.rb
+++ awake.rb
@@ -1,8 +1,8 @@
 class Awake < Formula
   desc "Keeps your computer from 💤"
   homepage "https://github.com/dambrisco/awake"
-  url "https://github.com/dambrisco/awake/archive/0.0.1.tar.gz"
-  sha256 "9f2b26c5dddc6cfda126465d794591f3be2fdb896c718ead28179cf3282d23f9"
+  url "https://github.com/dambrisco/awake/archive/0.0.2.tar.gz"
+  sha256 "19f6de21f4b58f545865854892ecb92bef91ea62a9c4215e2803724fb413e902"
 
   depends_on "jython"
 
