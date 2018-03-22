25c066bf066bdeaed638957e52d6d8aab29eb5b6
Dan Ambrisco <dambrisco@gmail.com>
2018-03-22 14:07:49 -0600

    Rearrange make cmd

--- vernemq.rb
+++ vernemq.rb
@@ -22,7 +22,7 @@ class Vernemq < Formula
     datadir.mkpath
 
     # system "make", "rel"
-    system "LDFLAGS=-L/usr/local/opt/openssl/lib", "CFLAGS=-I/usr/local/opt/openssl/include", "make", "rel"
+    system "make", "rel", "LDFLAGS=-L/usr/local/opt/openssl/lib", "CFLAGS=-I/usr/local/opt/openssl/include"
 
     bin.write_exec_script libexec/"bin/vernemq"
     bin.write_exec_script libexec/"bin/vmq-admin"
