b65efa399a2dc3bd10b2613e0024a125638cab4b
Dan Ambrisco <dambrisco@gmail.com>
2018-03-22 11:02:39 -0600

    Bump vernemq to 1.0.1

--- vernemq.rb
+++ vernemq.rb
@@ -1,14 +1,17 @@
 class Vernemq < Formula
   desc "Distributed MQTT Broker"
   homepage "https://vernemq.com"
-  url "https://s3.amazonaws.com/vernemq/vernemq-1.0.0-OSX-x86_64.tar.gz"
-  version "1.0.0"
-  sha256 "a6416d051435d81a17f262f359041966ff50defdefc97ce515a8ee3f5e3755f5"
+  # url "https://s3.amazonaws.com/vernemq/vernemq-1.0.0-OSX-x86_64.tar.gz"
+  url "https://github.com/erlio/vernemq/archive/1.0.1.tar.gz"
+  version "1.0.1"
+  # sha256 "a6416d051435d81a17f262f359041966ff50defdefc97ce515a8ee3f5e3755f5"
+  sha256 "cfcbb05a529dd7f51cdcf5d185eecd885194840c54209d563ee8f06606d2b572"
 
   bottle :unneeded
 
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
+  depends_on :erlang
 
   def install
     logdir = var + "log/vernemq"
@@ -18,6 +21,8 @@ class Vernemq < Formula
     libexec.install Dir["*"]
     logdir.mkpath
     datadir.mkpath
+
+    system "make", "rel"
 
     bin.write_exec_script libexec/"bin/vernemq"
     bin.write_exec_script libexec/"bin/vmq-admin"
