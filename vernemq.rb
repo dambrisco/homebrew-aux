d46f25402ca1682fcdf53ad373d8acd790913cc5
Dan Ambrisco <dambrisco@gmail.com>
2018-03-22 14:03:32 -0600

    sudo in a homebrew file?

--- vernemq.rb
+++ vernemq.rb
@@ -2,16 +2,16 @@ class Vernemq < Formula
   desc "Distributed MQTT Broker"
   homepage "https://vernemq.com"
   # url "https://s3.amazonaws.com/vernemq/vernemq-1.0.0-OSX-x86_64.tar.gz"
-  url "https://github.com/erlio/vernemq/archive/1.0.1.tar.gz"
-  version "1.0.1"
+  url "https://github.com/erlio/vernemq/archive/1.3.1.tar.gz"
   # sha256 "a6416d051435d81a17f262f359041966ff50defdefc97ce515a8ee3f5e3755f5"
-  sha256 "cfcbb05a529dd7f51cdcf5d185eecd885194840c54209d563ee8f06606d2b572"
+  sha256 "d6d156663087b4bbb61c7f3f2d620d80415c34f5bda4822d70a1e4628422bfe8"
 
   bottle :unneeded
 
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
   depends_on "erlang"
+  depends_on "openssl"
 
   def install
     logdir = var + "log/vernemq"
@@ -21,7 +21,8 @@ class Vernemq < Formula
     logdir.mkpath
     datadir.mkpath
 
-    system "make", "rel"
+    # system "make", "rel"
+    system "sudo", "LDFLAGS=-L/usr/local/opt/openssl/lib", "CFLAGS=-I/usr/local/opt/openssl/include", "make", "rel"
 
     bin.write_exec_script libexec/"bin/vernemq"
     bin.write_exec_script libexec/"bin/vmq-admin"
